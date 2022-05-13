2d5179e42ccc8b7e3c109a9b6d613ba0a72c9d4c
Lenovo <Lenovo@DESKTOP-MA9VB9U>
2022-05-13 12:22:05 +0530

    Java Case Study Framework Bankapp

---
+++ BankApp/src/com/tnsif/Application/MMBankFactory.java
@@ -0,0 +1,24 @@
+
+package com.tnsif.Application;
+
+import com.tnsif.Framework.BankFactory;
+
+public class MMBankFactory implements BankFactory
+{
+	public MMSavingAcc getNewSavingAccount(int accNo,String accNm,float accBal,boolean isSalaried)
+	{
+		MMSavingAcc mmsaving=new MMSavingAcc(accNo, accNm, accBal, isSalaried);
+		return mmsaving;
+	}
+
+	
+		public MMCurrentAcc getNewCurrentAccount(int accNo,String accNm,float accBal,float creditLimit)
+		{
+			MMCurrentAcc mmcurrent=new MMCurrentAcc(accNo, accNm, creditLimit, creditLimit);
+			return mmcurrent;
+		}
+
+}
+
+
+
